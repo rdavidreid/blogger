412401e06ee4cf32eeeaf4d6a3e6644ffd8da67c
David Reid <davidr@Davids-Air.localdomain>
2015-12-17 19:08:52 -0800

    Tagging feature completed

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,21 @@
+class TagsController < ApplicationController
+
+  def index
+    @tags = Tag.all
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def destroy
+    @tag = Tag.find(params[:id])
+    flash.notice = "'#{@tag.name}' was deleted"
+    @tag.destroy
+
+    redirect_to articles_path()
+  end
+
+
+
+end
